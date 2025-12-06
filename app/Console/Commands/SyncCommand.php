f30da58fa022361869c529147901b096ef8ec973
Lukas <lukasneuschl@gmail.com>
2025-12-06 19:28:33 +0100

    Add Residents to Sync command

--- app/Console/Commands/SyncCommand.php
+++ app/Console/Commands/SyncCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Planet;
+use App\Models\Resident;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
@@ -27,7 +28,7 @@ class SyncCommand extends Command
 
         do {
             try {
-                $response = Http::get($url)->json();
+                $response = Http::acceptJson()->get($url)->json();
             } catch (Throwable $e) {
                 $this->error('Failed to fetch Planets from SWAPI URL: ' . $url);
                 Log::error('Failed to fetch Planets from SWAPI', [
@@ -43,27 +44,13 @@ class SyncCommand extends Command
 
             $url = $response['next'];
 
-            foreach ($response['results'] as $inputPlanet) {
-                $planetId = Str::of($inputPlanet['url'])->after(config('services.swapi.base_url') . '/planets/')->before('/')->toInteger();
+            foreach ($response['results'] as $planetData) {
+                $planetId = Str::of($planetData['url'])->after(config('services.swapi.base_url') . '/planets/')->before('/')->toInteger();
 
                 try {
-                    $planet = Planet::updateOrCreate([
-                        'id' => $planetId,
-                        'name' => $inputPlanet['name'],
-                    ],
-                        [
-                            'diameter' => (int) $inputPlanet['diameter'],
-                            'rotation_period' => (int) $inputPlanet['rotation_period'],
-                            'orbital_period' => (int) $inputPlanet['orbital_period'],
-                            'gravity' => $inputPlanet['gravity'],
-                            'population' => (int) $inputPlanet['population'],
-                            'climate' => $inputPlanet['climate'],
-                            'terrain' => $inputPlanet['terrain'],
-                            'surface_water' => (int) $inputPlanet['surface_water'],
-                        ]
-                    );
+                    $planet = $this->upsertPlanet($planetId, $planetData);
                 } catch (Throwable $e) {
-                    $this->error('Failed to upsert Planet with ID ' . $planetId . ' - "' . $inputPlanet['name']);
+                    $this->error('Failed to upsert Planet with ID ' . $planetId . ' - "' . $planetData['name']);
                     Log::error('Failed to upsert Planet', [
                         'exception_message' => $e->getMessage(),
                         'exception_file' => $e->getFile(),
@@ -83,8 +70,55 @@ class SyncCommand extends Command
             }
         } while ($response['next'] !== null);
 
-        // Residents
+        $this->info('Planets synced.');
+        $this->info('Syncing Residents...');
 
+        // Residents
+        $url = config('services.swapi.base_url') . '/people';
+
+        do {
+            try {
+                $response = Http::acceptJson()->get($url)->json();
+            } catch (Throwable $e) {
+                $this->error('Failed to fetch Residents from SWAPI URL: ' . $url);
+                Log::error('Failed to fetch Residents from SWAPI', [
+                    'exception_message' => $e->getMessage(),
+                    'exception_file' => $e->getFile(),
+                    'exception_line' => $e->getLine(),
+                    'url' => $url,
+                    'command' => $this->getName(),
+                ]);
+
+                return self::FAILURE;
+            }
+
+            $url = $response['next'];
+
+            foreach ($response['results'] as $residentData) {
+                $residentId = Str::of($residentData['url'])->after(config('services.swapi.base_url') . '/people/')->before('/')->toInteger();
+
+                try {
+                    $resident = $this->upsertResident($residentId, $residentData);
+                } catch (Throwable $e) {
+                    $this->error('Failed to upsert Resident with ID ' . $residentId . ' - "' . $residentData['name']);
+                    Log::error('Failed to upsert Resident', [
+                        'exception_message' => $e->getMessage(),
+                        'exception_file' => $e->getFile(),
+                        'exception_line' => $e->getLine(),
+                        'command' => $this->getName(),
+                    ]);
+
+                    continue;
+                }
+
+                $this->line('Resident with ID ' . $resident->id . ' - "' . $resident->name . '" synced.');
+                Log::info('Resident synced', [
+                    'planet_id' => $resident->id,
+                    'planet_name' => $resident->name,
+                    'command' => $this->getName(),
+                ]);
+            }
+        } while ($response['next'] !== null);
 
         $this->info('Planets & Residents sync completed.');
         Log::info('Planets & Residents sync completed', [
@@ -92,5 +126,45 @@ class SyncCommand extends Command
         ]);
 
         return self::SUCCESS;
+    }
+
+    protected function upsertPlanet(int $planetId, array $data): Planet
+    {
+        return Planet::updateOrCreate(
+            [
+                'id' => $planetId,
+                'name' => $data['name'],
+            ],
+            [
+                'diameter' => (int) $data['diameter'],
+                'rotation_period' => (int) $data['rotation_period'],
+                'orbital_period' => (int) $data['orbital_period'],
+                'gravity' => $data['gravity'],
+                'population' => (int) $data['population'],
+                'climate' => $data['climate'],
+                'terrain' => $data['terrain'],
+                'surface_water' => (int) $data['surface_water'],
+            ]
+        );
+    }
+
+    protected function upsertResident(int $residentId, array $data): Resident
+    {
+        return Resident::updateOrCreate(
+            [
+                'id' => $residentId,
+            ],
+            [
+                'name' => $data['name'],
+                'birth_year' => $data['birth_year'],
+                'eye_color' => $data['eye_color'],
+                'gender' => $data['gender'],
+                'hair_color' => $data['hair_color'],
+                'height' => (int) $data['height'],
+                'mass' => (int) $data['mass'],
+                'skin_color' => $data['skin_color'],
+                'planet_id' => Str::of($data['homeworld'])->after(config('services.swapi.base_url') . '/planets/')->before('/')->toInteger(),
+            ]
+        );
     }
 }
--- database/migrations/2025_12_06_084138_create_residents_table.php
+++ database/migrations/2025_12_06_084138_create_residents_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->string('name')->nullable(false);
             $table->string('birth_year')->nullable(false)->comment('The birth year of the person, using the in-universe standard of BBY or ABY - Before the Battle of Yavin or After the Battle of Yavin. The Battle of Yavin is a battle that occurs at the end of Star Wars episode IV: A New Hope.');
             $table->string('eye_color')->nullable(false)->comment('The eye color of this person. Will be "unknown" if not known or "n/a" if the person does not have an eye.');
-            $table->string('gender')->nullable(false)->comment('The gender of this person. Either "Male", "Female" or "unknown", "n/a" if the person does not have a gender.'); // TODO: Enum?
+            $table->string('gender')->nullable(false)->comment('The gender of this person. Either "Male", "Female" or "unknown", "n/a" if the person does not have a gender.');
             $table->string('hair_color')->nullable(false)->comment('The hair color of this person. Will be "unknown" if not known or "n/a" if the person does not have hair.');
             $table->integer('height')->nullable(false)->unsigned()->comment('The height of the person in centimeters.');
             $table->integer('mass')->nullable(false)->unsigned()->comment('The mass of the person in kilograms.');
