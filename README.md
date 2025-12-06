10dd93ba4a2d15fb95d01caeaab4c1bebb4cd214
Lukas <lukasneuschl@gmail.com>
2025-12-06 08:49:30 +0100

    Add DB connection and images

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Template
+# Planets Explorer
+
+This old system connected to the internet will help you find out where in the universe you are located and how to get to your home planet.
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
     <div class="container">
-        <p>Index</p>
+        <img src="{{ Vite::asset('resources/images/logo.png') }}" class="img-fluid d-block mx-auto my-5" alt="Logo">
+
+        <h1 class="text-center mb-3">Welcome, Explorer!</h1>
+
+        <p class="text-center">This archaic system will help you find a way home.</p>
     </div>
 @endsection
