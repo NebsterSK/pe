4601c2f2c367363ca79510f61385babec8728fee
Lukas <lukasneuschl@gmail.com>
2025-12-07 18:17:51 +0100

    Add Laravel Sanctum and API auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,9 +13,19 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        User::factory()->create([
+        $admin = User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
+        ]);
+
+        DB::table('personal_access_tokens')->insert([
+            'tokenable_type' => User::class,
+            'tokenable_id' => $admin->id,
+            'name' => 'test',
+            'token' => 'f2d05d1d5b6f4c84373c413071fe0265bf571d1810f1da8b711de3389be3d7cc',
+            'abilities' => '["*"]',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,4 @@
 
 use App\Http\Controllers\Api\LogbookController;
 
-Route::resource('/logbook/entries', LogbookController::class)->only(['index', 'store'])->names('logbook.entries');
-//Route::get('/logbook/entries', [LogbookController::class, 'index'])->name('logbook.entries.index');
-//Route::post('/logbook/entries', [LogbookController::class, 'store'])->name('logbook.entries.store');
+Route::resource('/logbook/entries', LogbookController::class)->only(['index', 'store'])->middleware('auth:sanctum')->names('logbook.entries');
